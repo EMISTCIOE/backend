cdff50f7208bce1911c530239571f61340974575
ihamarch <whoisdinanath@gmail.com>
2023-06-22 21:16:28 +0545

    static and media fixes

--- tcioe/settings.py
+++ tcioe/settings.py
@@ -134,14 +134,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 if not DEBUG:
     STATIC_ROOT = 'static/'
     MEDIA_ROOT = 'media/'
 
 else:
-    MEDIA_URL = 'media/'
-    STATIC_URL = 'static/'
     STATICFILES_DIRS = [
         BASE_DIR / "static",
     ]
