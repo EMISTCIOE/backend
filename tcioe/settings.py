2f4453b4b2981551050b186d12df48ef70f481a9
ihamarch <whoisdinanath@gmail.com>
2023-06-22 21:13:44 +0545

    static and media fixes

--- tcioe/settings.py
+++ tcioe/settings.py
@@ -133,12 +133,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-MEDIA_ROOT = 'media/'
+
+
+if not DEBUG:
+    STATIC_ROOT = 'static/'
+    MEDIA_ROOT = 'media/'
+
+else:
+    MEDIA_URL = 'media/'
+    STATIC_URL = 'static/'
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
 
 
 # Default primary key field type
