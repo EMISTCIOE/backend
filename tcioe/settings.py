8149b0ca204524527a54075bc76fdc363f889410
Prashant Raj Bista <prashant.bista.18@gmail.com>
2023-07-20 22:25:13 +0545

    secret key added

--- tcioe/settings.py
+++ tcioe/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e77*)tnppp(8j-5v8!rf2sqddq*bm5mv#1s1kfytc#&sz=&@$x'
+SECRET_KEY = config(
+    'SECRET_KEY') or 'django-insecure-e77*)tnppp(8j-5v8!rf2sqddq*bm5mv#1s1kfytc#&sz=&@$x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
