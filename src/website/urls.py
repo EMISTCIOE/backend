6d02446f3a64f3a0718c205b97de353550dc2be7
Bibek Joshi <bibekjoshi34@gmail.com>
2025-07-19 10:22:05 +0545

    fix: bug import

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,24 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    HomeViewSet, SocialMediaViewSet, UnitViewSet,
-    ResourceViewSet, ImageGalleryViewSet, ImageViewSet,
-    CalendarViewSet, ReportViewSet,
-    ResourceSearchView, ImageSearchView
-)
+
 
 router = DefaultRouter()
-router.register(r'homepage', HomeViewSet)
-router.register(r'social-media', SocialMediaViewSet)
-router.register(r'units', UnitViewSet)
-router.register(r'resources', ResourceViewSet)
-router.register(r'galleries', ImageGalleryViewSet)
-router.register(r'images', ImageViewSet)
-router.register(r'calendars', CalendarViewSet)
-router.register(r'reports', ReportViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('resource-search/', ResourceSearchView.as_view(), name='resource-search'),
-    path('image-search/', ImageSearchView.as_view(), name='image-search'),
+    path("", include(router.urls)),
 ]
