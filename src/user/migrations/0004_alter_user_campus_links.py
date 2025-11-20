d3763eb33ec1581db5bb27b194367567eaceab78
Abhishek Panthee <abhishek.080bct002@tcioe.edu.np>
2025-11-20 16:10:47 +0545

    🐛 fix: Bug fixes | Author: Abhishek Panthee | Branch: main | 2025-11-20 16:10:47

--- src/user/migrations/0004_alter_user_campus_links.py
+++ src/user/migrations/0004_alter_user_campus_links.py
@@ -5,24 +5,12 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("user", "0002_user_classification_fields"),
-        ("website", "0001_initial"),
+        ("website", "0033_merge_20251119_1437"),
+        ("user", "0003_user_campus_section_user_campus_unit_alter_user_role"),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="user",
-            name="campus_section",
-            field=models.ForeignKey(
-                blank=True,
-                null=True,
-                on_delete=django.db.models.deletion.SET_NULL,
-                related_name="user_campus_sections",
-                to="website.campussection",
-                verbose_name="campus section",
-            ),
-        ),
-        migrations.AddField(
+        migrations.AlterField(
             model_name="user",
             name="campus_unit",
             field=models.ForeignKey(
@@ -32,6 +20,18 @@ class Migration(migrations.Migration):
                 related_name="user_campus_units",
                 to="website.campusunit",
                 verbose_name="campus unit",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="user",
+            name="campus_section",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="user_campus_sections",
+                to="website.campussection",
+                verbose_name="campus section",
             ),
         ),
     ]
